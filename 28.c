1988ff011b038ff639bfbce6e9a6877ffc5bd08a
Gaurav Rajpurohit <91660665+Gaurav-Rajpurohit@users.noreply.github.com>
2025-09-19 14:41:03 +0530

    Update 28.c

--- 28.c
+++ 28.c
@@ -17,8 +17,8 @@ Date: 29th Aug, 2025.
 int main(){
 	printf("The max real time priority(FIFO) = %d\n",sched_get_priority_max(SCHED_FIFO));
 	printf("The min real time priority(FIFO) = %d\n",sched_get_priority_min(SCHED_FIFO));
-	printf("The max real time priority(RR) = %d\n",sched_get_priority_max(SCHED_FIFO));
-	printf("The min real time priority(RR) = %d\n",sched_get_priority_min(SCHED_FIFO));
+	printf("The max real time priority(RR) = %d\n",sched_get_priority_max(SCHED_RR));
+	printf("The min real time priority(RR) = %d\n",sched_get_priority_min(SCHED_RR));
 	printf("The max real time priority = %d\n",sched_get_priority_max(SCHED_OTHER));
 	printf("The min real time priorit) = %d\n",sched_get_priority_min(SCHED_OTHER));
 }
